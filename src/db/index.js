5022d697881a3d4ad028ff511211a79985c63131
Atul jedge <atuljedge2801@gmail.com>
2024-01-11 18:39:18 +0530

    connect mongodb to atlas

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose"
 import {Db_Name} from "../constants.js"
 
-export const connectDb= async ()=>{
+export const connectDb= async ()=>{ 
     try{
-       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${Db_Name}`)
+     await mongoose.connect(`${process.env.MONGODB_URL}/${Db_Name}`)
        
        console.log(`\n MongoDB connected !! DB Host: `);
 
